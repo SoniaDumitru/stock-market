81d4bc95cd35e109909f65d792858188622b528c
SoniaDumitru <sonya_dumitru@yahoo.com>
2020-10-06 15:14:53 -0500

    commit for deploy

--- src/containers/PortofolioContainer.js
+++ src/containers/PortofolioContainer.js
@@ -7,31 +7,29 @@ class PortfolioContainer extends Component {
     console.log(this.props.myPortofolio)
     return (
       <div>
-          <h2>My Portfolio</h2>
-          <div>
-              <table className="table table-striped">
-              {this.props.myPortofolio.length > 0 ?
-                      <thead>
-                          <tr>
-                            <th scope="col">Company</th>
-                            <th scope="col">Symbol</th>
-                            <th scope="col">Current</th>
-                            <th scope="col">Low</th>
-                            <th scope="col">High</th>
-                            <th>Remove</th>
-
-                          </tr>
-                        </thead>
-                        : null}
-
-                    <tbody>
-                    {this.props.myPortofolio.map(stock =>
-                      <Stock
-                      stock={stock}
-                      removeFromPortofolio={this.props.removeFromPortofolio}
-                      parent='portofolio'/>)}
-                    </tbody>
-              </table>
+        <h2>My Portfolio</h2>
+        <div>
+          <table className="table table-striped">
+          {this.props.myPortofolio.length > 0 ?
+              <thead>
+                <tr>
+                  <th scope="col">Company</th>
+                  <th scope="col">Symbol</th>
+                  <th scope="col">Current</th>
+                  <th scope="col">Low</th>
+                  <th scope="col">High</th>
+                  <th>Remove</th>
+                </tr>
+                </thead>
+                : null}
+                <tbody>
+                {this.props.myPortofolio.map(stock =>
+                  <Stock
+                    stock={stock}
+                    removeFromPortofolio={this.props.removeFromPortofolio}
+                    parent='portofolio'/>)}
+                </tbody>
+            </table>
             </div>
       </div>
     );
